53e7e40f9303f272946e3afecec755d169502044
Nondumiso14 <Nondumisoshange3@gmail.com>
2025-02-07 02:51:06 -0800

    database

--- task_6.sql
+++ task_6.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE alx_book_store;
-CREATE TABLE Customers (
+CREATE TABLE Customer (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE,
     address TEXT);
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
       (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
       (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
